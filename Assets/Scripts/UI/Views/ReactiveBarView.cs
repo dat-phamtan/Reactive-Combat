7730551b232fa64fb61696dc250416e340a5a8f1
Jtad <dat519825@gmail.com>
2026-09-16 16:37:56 +0700

    bug

--- Assets/Scripts/UI/Views/ReactiveBarView.cs
+++ Assets/Scripts/UI/Views/ReactiveBarView.cs
@@ -4,10 +4,10 @@ using UnityEngine.UI;
 
 public class ReactiveBarView : MonoBehaviour
 {
-    [SerializeField] private Image _fillImage;
-    [SerializeField] private Color _normalColor = Color.white;
-    [SerializeField] private Color _perfectColor = Color.yellow;
-    [SerializeField] private Color _missColor = Color.red;
+    //[SerializeField] private Image _fillImage;
+    //[SerializeField] private Color _normalColor = Color.white;
+    //[SerializeField] private Color _perfectColor = Color.yellow;
+    //[SerializeField] private Color _missColor = Color.red;
     private float _duration;
 
     public void Show(float duration)
@@ -18,8 +18,8 @@ public class ReactiveBarView : MonoBehaviour
 
     public void UpdateProgress(float elapsed)
     {
-        if (_fillImage == null || _duration <= 0f)
-            return;
+        //if (_fillImage == null || _duration <= 0f)
+        //    return;
 
         float fillAmount = Mathf.Clamp01(elapsed / _duration);
         Debug.Log($"Update progress: {fillAmount}");
@@ -27,8 +27,8 @@ public class ReactiveBarView : MonoBehaviour
 
     public void ShowResult(TimingResult result)
     {
-        if (_fillImage == null)
-            return;
+        //if (_fillImage == null)
+        //    return;
         Debug.Log($"Show result: {result}");
     }
 
